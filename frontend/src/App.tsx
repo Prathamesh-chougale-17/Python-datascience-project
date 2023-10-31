79a0150c8321bebfee15cf22339115e4a31445d2
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-10-31 19:58:06 +0530

    added some typescript to the data from flask

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,27 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
+interface ResultProps {
+  result?: { branch_prediction: string; college_prediction: string };
+  error?: string;
+}
+
 function App() {
-  const [result, setResult] = useState("");
+  const [result, setResult] = useState<ResultProps>();
 
   useEffect(() => {
     axios
-      .get("http://localhost:5000/api/data-analysis")
+      .get<ResultProps>("http://localhost:5000/api/data-analysis")
       .then((response) => {
         const data = response.data;
         if (data.error) {
-          setResult(`Error: ${data.error}`);
+          setResult({
+            error: data.error,
+          });
         } else {
-          setResult(`Result: ${data.result}`);
+          setResult({
+            result: data.result,
+          });
         }
       })
       .catch((error) => console.error(error));
@@ -22,7 +31,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Data Analysis Result:</h1>
-        <p>{result}</p>
+        <p>{result?.result?.college_prediction}</p>
       </header>
     </div>
   );
